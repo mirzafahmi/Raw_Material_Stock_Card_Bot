2355c9a9c8e37b7ba193b652ce16148e0f88f844
mirza fahmi <mirzafahmi7@gmail.com>
2022-07-26 22:05:12 +0800

    finished the naming variable dynamically

--- Raw_Data/test2.py
+++ Raw_Data/test2.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import re
 
 class ProDetect:
     def __init__(self, product_name, tests_per_box):
@@ -7,9 +6,12 @@ class ProDetect:
         self.tests_per_box = tests_per_box
 
     def min_potential_produced(self):
+
+        #dynamically read the excel file from the name of the product
         product_data = pd.read_excel(f'Raw_Data/F037_For_{self.product_name}.xlsx', sheet_name = None)
+
+        #get the name of the sheet to differentiate between uncut_sheet components and others as some products has combinations thus need different function for the uncut_sheet sheet
         sheet_name = list(product_data.keys())
-        #print(product_data["Cassette"])
         all_uncut_sheets = [i for i in sheet_name if "Sheet" in i]
         other_components = [i for i in sheet_name if "Sheet" not in i]
 
@@ -18,17 +20,23 @@ class ProDetect:
         product_tests_per_uncut_sheet = 70
         potential_produced_product_raw_materials = {}
 
+        #create for loop to dynamically go through all the uncut_sheet sheet
+
         for sheet in all_uncut_sheets:
+
+            #globals() made the dynamic variable name possible
+
             globals()[f"product_{sheet}_uncut_sheet_raw"] = product_data[f"{sheet}"]
             
             #trim unnecessary excel cell and assign to columns name
             globals()[f"product_{sheet}_uncut_sheet"] = globals()[f"product_{sheet}_uncut_sheet_raw"].iloc[10:]
             globals()[f"product_{sheet}_uncut_sheet"].columns = globals()[f"product_{sheet}_uncut_sheet_raw"].iloc[8]
             
-            #fomula to calculate potential produced goods based on uncutsheet
+            #fomula to calculate potential produced goods based on "uncut sheet", it will read the last value in "Received" column
             globals()[f"potential_produced_product_by_{sheet}_uncut_sheet"] = globals()[f"product_{sheet}_uncut_sheet"]["Received"].iloc[-1] * product_tests_per_uncut_sheet / product_tests_per_box
-            x = f"potential_produced_{self.product_name}_by_{sheet}_uncut_sheet"
-            potential_produced_product_raw_materials[x] = globals()[f"potential_produced_product_by_{sheet}_uncut_sheet"]
+            potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_{sheet}_uncut_sheet"] = globals()[f"potential_produced_product_by_{sheet}_uncut_sheet"]
+
+        #for loop for all other components
 
         for sheet in other_components:
             if (sheet == "Cassette"):
@@ -45,7 +53,7 @@ class ProDetect:
                 adjusted_columns_name[5] = "Panels Issue"
                 product_cassette.columns = adjusted_columns_name
 
-                #formula to calculate potential produced goods based on cap and panel
+                #formula to calculate potential produced goods based on "cap" and "panel", it will read the last value in "Received" column
                 potential_produced_product_by_cap = product_cassette["Caps Received"].iloc[-1] / product_tests_per_box
                 potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_cap"] = potential_produced_product_by_cap
                 potential_produced_product_by_panel = product_cassette["Panels Received"].iloc[-1] / product_tests_per_box
@@ -59,6 +67,7 @@ class ProDetect:
             product_pipette = product_pipette_raw.iloc[10:]
             product_pipette.columns = product_pipette_raw.iloc[8]
 
+            #formula to calculate potential produced goods based on "pipette", it will read the last value in "Received" column
             potential_produced_product_by_pipette = (product_pipette["Received"].iloc[-1] / product_tests_per_box)
             potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_pipette"] = potential_produced_product_by_pipette
 
@@ -69,12 +78,20 @@ class ProDetect:
             product_buffer = product_buffer_raw.iloc[10:]
             product_buffer.columns = product_buffer_raw.iloc[8]
 
+            #formula to calculate potential produced goods based on "buffer", it will read the last value in "Received" column
             potential_produced_product_by_buffer = (product_buffer["Received"].iloc[-1] / product_tests_per_box)
             potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_buffer"] = potential_produced_product_by_buffer
 
-        print(potential_produced_product_raw_materials)
+        #get the "min" component's key to ease tp read the result
+        min_boxes_of_product_key = min(potential_produced_product_raw_materials, key = potential_produced_product_raw_materials.get)
+        
+        #
+        min_boxes_of_product = f'{min_boxes_of_product_key} : {potential_produced_product_raw_materials[min_boxes_of_product_key]}'
+
+        print([f'{key} -> {value}' for key,value in potential_produced_product_raw_materials.items()])
+        print(min_boxes_of_product)
         
 
-pr = ProDetect("TEST", 25)
+pr = ProDetect("PR_DOA_5", 25)
 
 print(pr.min_potential_produced())
