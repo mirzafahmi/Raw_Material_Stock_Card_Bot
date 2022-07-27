910219b2814979df172cd71eb1fc6c88a754aca5
mirza fahmi <mirzafahmi7@gmail.com>
2022-07-27 22:12:02 +0800

    refactoring the function for other sheets than uncut sheet sheet

--- Raw_Data/test2.py
+++ Raw_Data/test2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class ProDetect:
     def __init__(self, product_name, tests_per_box):
@@ -13,7 +14,9 @@ class ProDetect:
         #get the name of the sheet to differentiate between uncut_sheet components and others as some products has combinations thus need different function for the uncut_sheet sheet
         sheet_name = list(product_data.keys())
         all_uncut_sheets = [i for i in sheet_name if "Sheet" in i]
-        other_components = [i for i in sheet_name if "Sheet" not in i]
+        all_cassettes = [i for i in sheet_name if "Cassette" in i]
+        all_buffers = [i for i in sheet_name if "Buffer" in i]
+        other_components = [i for i in sheet_name if i not in np.concatenate((all_uncut_sheets, all_cassettes, all_buffers))]
 
         #create variable
         product_tests_per_box = self.tests_per_box
@@ -25,7 +28,6 @@ class ProDetect:
         for sheet in all_uncut_sheets:
 
             #globals() made the dynamic variable name possible
-
             globals()[f"product_{sheet}_raw"] = product_data[f"{sheet}"]
             
             #trim unnecessary excel cell and assign to columns name
@@ -38,65 +40,65 @@ class ProDetect:
 
         #for loop for all other components
 
-        for sheet in other_components:
-            if (sheet == "Cassette"):
-                product_cassette_raw = product_data["Cassette"]
+        for sheet in all_cassettes:
+            #globals() made the dynamic variable name possible
+            globals()[f"product_{sheet}_raw"] = product_data[f"{sheet}"]
 
+            #trim unnecessary excel cell and assign to columns name
+            globals()[f"product_{sheet}"] = globals()[f"product_{sheet}_raw"].iloc[10:]
+            adjusted_columns_name = globals()[f"product_{sheet}_raw"].iloc[8]
+
+            #renaming the blank columns due to multi-index column
+            adjusted_columns_name[1] = "Caps Received"
+            adjusted_columns_name[2] = "Caps Issue"
+            adjusted_columns_name[4] = "Panels Received"
+            adjusted_columns_name[5] = "Panels Issue"
+            globals()[f"product_{sheet}"].columns = adjusted_columns_name
+
+            #formula to calculate potential produced goods based on "cap" and "panel", it will read the last value in "Received" column
+            globals()[f"potential_produced_product_by_{sheet}_caps"] = globals()[f"product_{sheet}"]["Caps Received"].iloc[-1] / product_tests_per_box
+            potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_{sheet}_caps"] = globals()[f"potential_produced_product_by_{sheet}_caps"]
+            globals()[f"potential_produced_product_by_{sheet}_panels"] = globals()[f"product_{sheet}"]["Panels Received"].iloc[-1] / product_tests_per_box
+            potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_{sheet}_panels"] = globals()[f"potential_produced_product_by_{sheet}_panels"]
+
+            for sheet in all_buffers:
+                #globals() made the dynamic variable name possible
+                globals()[f"product_{sheet}_raw"] = product_data[f"{sheet}"]
+                
                 #trim unnecessary excel cell and assign to columns name
-                product_cassette = product_cassette_raw.iloc[10:]
-                adjusted_columns_name = product_cassette_raw.iloc[8]
+                globals()[f"product_{sheet}"] = globals()[f"product_{sheet}_raw"].iloc[10:]
+                globals()[f"product_{sheet}"].columns = globals()[f"product_{sheet}_raw"].iloc[8]
 
-                #renaming the blank columns due to multi-index column
-                adjusted_columns_name[1] = "Caps Received"
-                adjusted_columns_name[2] = "Caps Issue"
-                adjusted_columns_name[4] = "Panels Received"
-                adjusted_columns_name[5] = "Panels Issue"
-                product_cassette.columns = adjusted_columns_name
+                
+                globals()[f"potential_produced_product_by_{sheet}"] = globals()[f"product_{sheet}"]["Received"].iloc[-1] / product_tests_per_box #different products have diffrent quantity buffer
+                potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_{sheet}"] = globals()[f"potential_produced_product_by_{sheet}"]
+            
+            for sheet in other_components:
+                #globals() made the dynamic variable name possible
+                globals()[f"product_{sheet}_raw"] = product_data[f"{sheet}"]
+                
+                #trim unnecessary excel cell and assign to columns name
+                globals()[f"product_{sheet}"] = globals()[f"product_{sheet}_raw"].iloc[10:]
+                globals()[f"product_{sheet}"].columns = globals()[f"product_{sheet}_raw"].iloc[8]
 
-                #formula to calculate potential produced goods based on "cap" and "panel", it will read the last value in "Received" column
-                potential_produced_product_by_cap = product_cassette["Caps Received"].iloc[-1] / product_tests_per_box
-                potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_cap"] = potential_produced_product_by_cap
-                potential_produced_product_by_panel = product_cassette["Panels Received"].iloc[-1] / product_tests_per_box
-                potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_panel"] = potential_produced_product_by_panel
+                globals()[f"potential_produced_product_by_{sheet}"] = globals()[f"product_{sheet}"]["Received"].iloc[-1] / product_tests_per_box
+                potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_{sheet}"] = globals()[f"potential_produced_product_by_{sheet}"]
 
-
-        if (sheet == "Pipette"):
-            product_pipette_raw = product_data["Pipette"]
-
-            #trim unnecessary excel cell and assign to columns name
-            product_pipette = product_pipette_raw.iloc[10:]
-            product_pipette.columns = product_pipette_raw.iloc[8]
-
-            #formula to calculate potential produced goods based on "pipette", it will read the last value in "Received" column
-            potential_produced_product_by_pipette = (product_pipette["Received"].iloc[-1] / product_tests_per_box)
-            potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_pipette"] = potential_produced_product_by_pipette
-
-        if (sheet == "Buffer"):
-            product_buffer_raw = product_data["Buffer"]
-
-            #trim unnecessary excel cell and assign to columns name
-            product_buffer = product_buffer_raw.iloc[10:]
-            product_buffer.columns = product_buffer_raw.iloc[8]
-
-            #formula to calculate potential produced goods based on "buffer", it will read the last value in "Received" column
-            potential_produced_product_by_buffer = (product_buffer["Received"].iloc[-1] / product_tests_per_box)
-            potential_produced_product_raw_materials[f"potential_produced_{self.product_name}_by_buffer"] = potential_produced_product_by_buffer
 
         #get the "min" component's key to ease tp read the result
         min_boxes_of_product_key = min(potential_produced_product_raw_materials, key = potential_produced_product_raw_materials.get)
         
-        #
         #min_boxes_of_product = f'{min_boxes_of_product_key} -> {potential_produced_product_raw_materials[min_boxes_of_product_key]}'
-        min_boxes_of_product = {min_boxes_of_product_key:potential_produced_product_raw_materials[min_boxes_of_product_key]}
+        min_boxes_of_product = {min_boxes_of_product_key:potential_produced_product_raw_materials[min_boxes_of_product_key]} #variant of above code
         
-        print(f'{self.product_name}' + '\n')
-
+        #print(f'{self.product_name}' + '\n')
+        print('Raw Material Output (in boxes): ')
         for key,value in potential_produced_product_raw_materials.items():
-            x = f'{key} -> {value}' + '\n'
+            x = f'{key} -> {value}'
             print(x)
         #print([f'{key} -> {value}' for key,value in potential_produced_product_raw_materials.items()])
-        print(min_boxes_of_product)
-        #print(potential_produced_product_by_pipette)
+
+        print('\n' f'Bottlenecked Raw Material (in boxes): ' '\n' f'{min_boxes_of_product}')
         
 
 PR_CHK = ProDetect("PR_CHK", 25)
